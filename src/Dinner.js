2cd548234e17ac8f092259c03fef695612d83f8c
Muneeb926595 <31837560+Muneeb926595@users.noreply.github.com>
2020-06-08 11:12:25 +0500

    Added Animations

--- src/Dinner.js
+++ src/Dinner.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './App.css';
 
 function Dinner(props)
 {
     return(
         <div>
-            <h1>We are serving today {props.dishName}</h1>
-            <h1>We are also serving sweet dish {props.sweetName}</h1>
+            <h1 className="fromleft">We are serving today {props.dishName}</h1>
+            <h1 className="fromright">We are also serving sweet dish {props.sweetName}</h1>
         </div>
     )
 }
